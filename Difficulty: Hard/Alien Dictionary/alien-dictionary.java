86597048968bf35f98d87f4fe3f656977b477055
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-08-22 20:29:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.java
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = Integer.parseInt(sc.next());
+        while (t-- > 0) {
+            int n = Integer.parseInt(sc.next());
+            int k = Integer.parseInt(sc.next());
+
+            String[] words = new String[n];
+
+            for (int i = 0; i < n; i++) {
+                words[i] = sc.next();
+            }
+
+            Solution ob = new Solution();
+            //  System.out.println(T.findOrder(words,k));
+            String order = ob.findOrder(words, n, k);
+            if (order.length() == 0) {
+                System.out.println(0);
+                continue;
+            }
+            if (order.length() != k) {
+                System.out.println("INCOMPLETE");
+                return;
+            }
+            String temp[] = new String[n];
+            for (int i = 0; i < n; i++) temp[i] = words[i];
+
+            Arrays.sort(temp, new Comparator<String>() {
+                @Override
+                public int compare(String a, String b) {
+                    int index1 = 0;
+                    int index2 = 0;
+                    for (int i = 0;
+                         i < Math.min(a.length(), b.length()) && index1 == index2;
+                         i++) {
+                        index1 = order.indexOf(a.charAt(i));
+                        index2 = order.indexOf(b.charAt(i));
+                    }
+
+                    if (index1 == index2 && a.length() != b.length()) {
+                        if (a.length() < b.length())
+                            return -1;
+                        else
+                            return 1;
+                    }
+
+                    if (index1 < index2)
+                        return -1;
+                    else
+                        return 1;
+                }
+            });
+
+            int flag = 1;
+            for (int i = 0; i < n; i++) {
+                if (!words[i].equals(temp[i])) {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            System.out.println(flag);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public String findOrder(String[] words, int n, int k) {
+        boolean[][] g = new boolean[26][26];
+        boolean[] s = new boolean[26];
+        int cnt = 0;
+        int nn = words.length;
+        for (int i = 0; i < nn - 1; ++i) {
+            for (char c : words[i].toCharArray()) {
+                if (cnt == 26) {
+                    break;
+                }
+                c -= 'a';
+                if (!s[c]) {
+                    ++cnt;
+                    s[c] = true;
+                }
+            }
+            int m = words[i].length();
+            for (int j = 0; j < m; ++j) {
+                if (j >= words[i + 1].length()) {
+                    return "";
+                }
+                char c1 = words[i].charAt(j), c2 = words[i + 1].charAt(j);
+                if (c1 == c2) {
+                    continue;
+                }
+                if (g[c2 - 'a'][c1 - 'a']) {
+                    return "";
+                }
+                g[c1 - 'a'][c2 - 'a'] = true;
+                break;
+            }
+        }
+        for (char c : words[nn - 1].toCharArray()) {
+            if (cnt == 26) {
+                break;
+            }
+            c -= 'a';
+            if (!s[c]) {
+                ++cnt;
+                s[c] = true;
+            }
+        }
+
+        int[] indegree = new int[26];
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                if (i != j && s[i] && s[j] && g[i][j]) {
+                    ++indegree[j];
+                }
+            }
+        }
+        Deque<Integer> q = new LinkedList<>();
+        for (int i = 0; i < 26; ++i) {
+            if (s[i] && indegree[i] == 0) {
+                q.offerLast(i);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        while (!q.isEmpty()) {
+            int t = q.pollFirst();
+            ans.append((char)(t + 'a'));
+            for (int i = 0; i < 26; ++i) {
+                if (i != t && s[i] && g[t][i]) {
+                    if (--indegree[i] == 0) {
+                        q.offerLast(i);
+                    }
+                }
+            }
+        }
+        return ans.length() < cnt ? "" : ans.toString();
+    }
+}
