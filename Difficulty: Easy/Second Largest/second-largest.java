78fca2c1619a5921cf0f4a654dc238b2eb9c8ac1
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-11-25 16:10:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -10,15 +10,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
         while (t-- > 0) {
-            List<Integer> arr = new ArrayList<>();
-            String input = sc.nextLine();
-            Scanner ss = new Scanner(input);
-            while (ss.hasNextInt()) {
-                arr.add(ss.nextInt());
-            }
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
             Solution ob = new Solution();
-            int ans = ob.print2largest(arr);
+            int ans = ob.getSecondLargest(arr);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -29,25 +27,40 @@ public class Main {
 // User function Template for Java
 
 class Solution {
-    public int print2largest(List<Integer> arr) {
+    public int getSecondLargest(int[] arr) {
         
-        int fm = -1;
-        int sm = -1;
+        //created two varaible that holds the first max. and second max
+        int firstMax = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
         
-        for(int i=0; i<arr.size(); i++)
+        //size of array.
+        int n = arr.length;
+        
+        //traverse array and find the answer.
+        for(int i=0; i<n; i++)
         {
-            int cur = arr.get(i);
-            if(cur > fm)
+            //current element.
+            int currentElement = arr[i];
+            
+            //if current element is greater than the firstMax.
+            if(currentElement > firstMax)
             {
-                sm = fm;
-                fm = cur;
+                secondMax = firstMax;
+                firstMax = currentElement;
             }
-            else if(cur < fm && cur > sm)
+            else if(secondMax < currentElement && firstMax != currentElement)//if current element is greater than the second max.
             {
-                sm = cur;
+                secondMax = currentElement;
             }
+            
         }
         
-        return sm;
+        
+        //if firstMax and secondMax is same then we return -1 as per question.
+        if(firstMax == secondMax) return -1;
+        
+        if(secondMax == Integer.MIN_VALUE) return -1;
+        //else we return the secondMax element.
+        return secondMax;
     }
 }
